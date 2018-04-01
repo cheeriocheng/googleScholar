2c4f2ed2ebef538aa0b1354c9fdb5804586af331
Cheng <cheeriocheng@gmail.com>
2018-04-01 14:53:30 -0700

    export txt instead of csv

--- googleScholar.py
+++ googleScholar.py
@@ -7,38 +7,105 @@ from time import sleep
 import csv
 
 driver = webdriver.Chrome()
+#the first page of the scholar citation page
 driver.get("https://scholar.google.com/scholar?cites=7079466816456614288&as_sdt=2005&sciodt=0,5&hl=en")
-# assert "Python" in driver.title
+
+
 #gs_ri  citation
-#<h3 class="gs_rt" ontouchstart="gs_evt_dsp(event)"><a href="https://dl.acm.org/citation.cfm?id=2557005" data-clk="hl=en&amp;sa=T&amp;ct=res&amp;cd=1&amp;ei=ygPAWuXMNqvXjgTTx62gBg">faBrickation: fast 3D printing of functional objects by integrating construction kit building blocks</a></h3>
+#gs_rt titles
 #gs_a authors 
 
 
 index = 1 
 
+def dequote(s):
+    """
+    If a string has single or double quotes around it, remove them.
+    Make sure the pair of quotes match.
+    If a matching pair of quotes is not found, return the string unchanged.
+    """
+    if (s[0] == s[-1]) and s.startswith(("'", '"')):
+        return s[1:-1]
+    return s
+
 def webToCSV():
 	global index
 	citations = driver.find_elements_by_class_name("gs_ri")
 	for c in citations:
-		title = c.find_element_by_xpath('.//h3[@class="gs_rt"]')
-		# authors = c.find_elements_by_xpath('.//div[@class="gs_a"]/a')  #dont include the procceding name 
-		authors = c.find_elements_by_xpath('.//div[@class="gs_a"]')
+		title = c.find_element_by_xpath('.//h3[@class="gs_rt"]').text
+		title = dequote(title)
+		
+		authors = c.find_elements_by_xpath('.//div[@class="gs_a"]')[0].text
+		'''authors = c.find_elements_by_xpath('.//div[@class="gs_a"]/a')  #dont include the procceding name 
 		s=''
 		for a in authors: 
 			s += a.text 
-		writer.writerow([str(index)] + [title.text] + [s] )
+			s += ', '
+		if len(authors)>0:
+			s = s[0:-2]
+		'''
+		writer.writerow([str(index)] + [title])
+		writer.writerow([authors])
+		writer.writerow('\n') #spacer between entries
 		index += 1
 
 
-with open('citations.csv', 'w') as csvfile:
-	writer = csv.writer(csvfile, delimiter=' ')
+
+# with open('citations.csv', 'w',newline='') as csvfile:
+
+# 	writer = csv.writer(csvfile)
+# 	while True: 
+# 		webToCSV()
+# 		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+# 		sleep(1) #time for the page to scroll down and make the next button visiable
+# 		# click next button
+# 		try:
+# 			# Note that when debug window is open on the side, the HTML elements are different! 
+# 			# Use debug window on the bottom to identify the class name.
+# 			# submit_button = driver.find_elements_by_xpath('//*[@id="gs_nm"]/button')[1] #  button for next page
+# 			next_button = driver.find_element_by_class_name("gs_ico_nav_next")
+# 			next_button.click()
+# 		except:
+# 			driver.close()
+# 			break
+
+def webToTXT():
+	global index
+	citations = driver.find_elements_by_class_name("gs_ri")
+	for c in citations:
+		title = c.find_element_by_xpath('.//h3[@class="gs_rt"]').text
+		title = dequote(title)
+		
+		authors = c.find_elements_by_xpath('.//div[@class="gs_a"]')[0].text
+		'''authors = c.find_elements_by_xpath('.//div[@class="gs_a"]/a')  #dont include the procceding name 
+		s=''
+		for a in authors: 
+			s += a.text 
+			s += ', '
+		if len(authors)>0:
+			s = s[0:-2]
+		'''
+		file.write(str(index) + ' "' + title + '"')
+		file.write('\n')
+		file.write(authors)
+		file.write('\n')
+		file.write('\n')
+
+		index += 1
+
+
+with open('citations.txt', 'w') as file:
+# with open('citations.csv', 'w',newline='') as csvfile:
+
 	while True: 
-		webToCSV()
+		webToTXT()
 		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		sleep(2)
+		sleep(1) #time for the page to scroll down and make the next button visiable
 		# click next button
-		# submit_button = driver.find_elements_by_xpath('//*[@id="gs_nm"]/button')[1] #  button for next page
 		try:
+			# Note that when debug window is open on the side, the HTML elements are different! 
+			# Use debug window on the bottom to identify the class name.
+			# submit_button = driver.find_elements_by_xpath('//*[@id="gs_nm"]/button')[1] #  button for next page
 			next_button = driver.find_element_by_class_name("gs_ico_nav_next")
 			next_button.click()
 		except:
@@ -47,21 +114,3 @@ with open('citations.csv', 'w') as csvfile:
 
 
 
-
-
-	# citations = driver.find_elements_by_class_name("gs_ri")
-	# for c in citations:
-	# 	title = c.find_element_by_xpath('.//h3[@class="gs_rt"]')
-	# 	# authors = c.find_elements_by_xpath('.//div[@class="gs_a"]/a')  #dont include the procceding name 
-	# 	authors = c.find_elements_by_xpath('.//div[@class="gs_a"]')
-	# 	s=''
-	# 	for a in authors: 
-	# 		s += a.text 
-	# 	writer.writerow([str(index)] + [title.text] + [s] )
-	# 	index += 1
-
- 
-
-
-
-# persons.append({'title': title, 'company': company})
