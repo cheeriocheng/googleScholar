147657840bc905ad2be57c4febdf6c9b889e543f
Cheng <cheeriocheng@gmail.com>
2018-04-01 20:47:45 -0700

    load all papers from profile page

--- googleScholar.py
+++ googleScholar.py
@@ -5,16 +5,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep 
 import csv
+import random 
+
 
 driver = webdriver.Chrome()
-#the first page of the scholar citation page
-driver.get("https://scholar.google.com/scholar?cites=7079466816456614288&as_sdt=2005&sciodt=0,5&hl=en")
-
 
 #gs_ri  citation
 #gs_rt titles
 #gs_a authors 
-
 
 index = 1 
 
@@ -36,21 +34,16 @@ def webToCSV():
 		title = dequote(title)
 		
 		authors = c.find_elements_by_xpath('.//div[@class="gs_a"]')[0].text
-		'''authors = c.find_elements_by_xpath('.//div[@class="gs_a"]/a')  #dont include the procceding name 
-		s=''
-		for a in authors: 
-			s += a.text 
-			s += ', '
-		if len(authors)>0:
-			s = s[0:-2]
-		'''
+		#authors = c.find_elements_by_xpath('.//div[@class="gs_a"]/a')  #dont include the procceding name 
 		writer.writerow([str(index)] + [title])
 		writer.writerow([authors])
 		writer.writerow('\n') #spacer between entries
 		index += 1
 
 
-
+'''
+EXPORT CSV 
+'''
 # with open('citations.csv', 'w',newline='') as csvfile:
 
 # 	writer = csv.writer(csvfile)
@@ -90,17 +83,17 @@ def webToTXT():
 		file.write(authors)
 		file.write('\n')
 		file.write('\n')
-
+		print("Citations found: " + str(index))
 		index += 1
 
-
-with open('citations.txt', 'w') as file:
-# with open('citations.csv', 'w',newline='') as csvfile:
-
+def loadAllCitationsFromLink(link):
+	#the first page of the scholar citation page
+	driver.get(link)
+	input("when webpage is ready, click enter: ")
 	while True: 
 		webToTXT()
 		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		sleep(1) #time for the page to scroll down and make the next button visiable
+		sleep(random.random()*50) #time for the page to scroll down and make the next button visiable
 		# click next button
 		try:
 			# Note that when debug window is open on the side, the HTML elements are different! 
@@ -109,8 +102,31 @@ with open('citations.txt', 'w') as file:
 			next_button = driver.find_element_by_class_name("gs_ico_nav_next")
 			next_button.click()
 		except:
-			driver.close()
+			# driver.close()
 			break
+
+
+'''
+EXPORT TXT
+'''
+with open('citations.txt', 'w') as file:
+	#this is the main google page
+	driver.get("https://scholar.google.com/citations?user=aaTHLnkAAAAJ&hl=en&oi=sra")
+	input("when webpage is ready, click enter: ")
+	#find all citation links
+	links = []
+	for l in driver.find_elements_by_xpath('.//a[@class="gsc_a_ac gs_ibl"]'):
+		if l.text != '' :
+			print(l.get_attribute('href'))
+		# loadAllCitationsFromLink("https://scholar.google.com/scholar?cites=7079466816456614288&as_sdt=2005&sciodt=0,5&hl=en")
+			links.append(l.get_attribute('href'))		
+
+	for l in links:
+		sleep(random.random()*30)
+		loadAllCitationsFromLink(l)	
+	
+	file.close()
+	driver.close()
 
 
 
